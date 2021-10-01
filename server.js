8f61b68685dcd3a2b5487098de7faa752f69fc85
Omoore94 <omoh.afekhume1@gmail.com>
2021-10-01 23:26:05 +0100

    added the first GET request

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.listen(5000, function() {
-console.log("this server is up and runnning")
+
+const mockUserData = [
+    {name:'Paul'},
+    {name:'Osamose'}
+]
+
+
+app.get('/users', function(req, res) {
+        res.json({
+            success: true, 
+            message: 'successfully got users. Nice work!',
+            users: mockUserData
+        })
 });
+
+
+app.listen(5000, function() {
+    console.log("this server is up and runnning")
+    });
